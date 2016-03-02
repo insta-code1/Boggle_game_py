0d098b8d35bce98606d165ec43212d5bed3491df
Eoin <eoinkane1@gmail.com>
2016-03-02 12:17:14 +0000

    implemented grid positions

--- boggle.py
+++ boggle.py
@@ -1,11 +1,23 @@
 from random import choice
 from string import ascii_uppercase
 
-size = 2, 2
 
-grid = {(x, y): choice(ascii_uppercase)
-        for x in range(size[0])
-        for y in
-        range(size[1])}
+def get_grid():
+    return {(x, y): choice(ascii_uppercase) for x in range(X) for y in range(Y)}
 
-print grid
+
+def get_neighbours():
+    neighbours = {}
+
+    for position in grid:
+        x, y = position
+        positions = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x + 1, y),
+                    (x + 1, y + 1), (x, y + 1), (x - 1, y + 1), (x - 1, y)]
+
+        neighbours[position] = [p for p in positions if 0 <= p[0] < X and 0 <= p[1] < Y]
+    return neighbours
+
+
+size = X, Y = 2, 2
+grid = get_grid()
+neighbours = get_neighbours()
